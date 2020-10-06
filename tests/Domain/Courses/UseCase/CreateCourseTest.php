627e1242477d2323cabb835ce8bf9edec2adfcdd
Lior Chamla <lchamla@gmail.com>
2020-10-07 01:58:21 +0200

    adding tests and code for course creation in the domain

---
+++ tests/Domain/Courses/UseCase/CreateCourseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Tests\Domain\Courses\Adapters\InMemoryCourseRepository;
+use Domain\Courses\Entity\Course;
+use Domain\Courses\UseCase\CreateCourse;
+use Domain\Courses\UseCase\RequestModel\CreateCourseRequest;
+
+use function PHPUnit\Framework\assertInstanceOf;
+use function PHPUnit\Framework\assertNotNull;
+
+it("can create a course with good data", function () {
+    // Setup :
+    $courseRepository = new InMemoryCourseRepository;
+
+    // Given we have good data
+    $data = [
+        'title' => "My Course Title",
+        'price' => 4000
+    ];
+
+    // When we call the usecase
+    $useCase = new CreateCourse($courseRepository);
+    $view = $useCase->execute(new CreateCourseRequest($data));
+
+    // Then a new course should be created
+    assertInstanceOf(Course::class, $view->course);
+
+    // And we can find it in the database
+    assertNotNull($courseRepository->findOneOrNull($view->course->uuid));
+});
